755f1bb7957e911cd17a41596ceea338650669cf
jamesxv7 <jamesxv7@gmail.com>
2015-09-20 21:06:15 -0400

    Release to public

---
+++ server.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+
+var port = process.env.port || 1337;
+
+var app = express();
+
+app.use(express.static('public'));
+
+var server = app.listen(port, function() {
+    var host = server.address().address;
+    
+    console.log('Hey... I am at http://%s:%s', host, port)
+})
