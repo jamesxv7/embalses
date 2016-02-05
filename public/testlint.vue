dd1c5e6024e9c5cd5b85732b9b40155f38c73825
Jaime Olmo <jamesxv7@gmail.com>
2016-02-05 10:44:59 -0400

    Clean up main.js file name

---
+++ public/testlint.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<input type="email" placeholder="Email validator" />
+	</div>	
+</template>
+
+<script>
+	import 
+</script>
+
+<style>
+	.message {
+		border: 1px solid red;
+	}
+</style>
