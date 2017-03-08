075947908d9179bfb1c6305eade4d2a5ae91042f
jamesxv7 <jamesxv7@gmail.com>
2017-03-08 16:41:42 -0400

    Initial work to convert into components

---
+++ src/components/search.vue
@@ -0,0 +1,11 @@
+<template>
+  <form id="search">
+    <input class="form-control" placeholder="Filtrar información..." name="q" v-model="filterKey">
+  </form>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/components/sort.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="btn-group btn-group-justified" role="group">
+  <div class="btn-group" role="group">
+    <button type="button" class="btn btn-default" v-on:click="sortBy('city')">Pueblo</button>
+  </div>
+  <div class="btn-group" role="group">
+    <button type="button" class="btn btn-default" v-on:click="sortBy('currentLevel')">Nivel</button>
+  </div>
+  <div class="btn-group" role="group">
+    <button type="button" class="btn btn-default" v-on:click="sortBy('status')">Status</button>
+  </div>
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
