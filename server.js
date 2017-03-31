c4ff9e6a7fb863d7a2c5150daff58b20493116b5
jamesxv7 <jamesxv7@gmail.com>
2017-03-31 15:46:33 -0400

    Server.js needed in Azure

---
+++ server.js
@@ -0,0 +1,13 @@
+﻿const express = require('express')
+
+const port = process.env.port || 1337
+
+const app = express()
+
+app.use(express.static('public'))
+
+let server = app.listen(port, function () {
+  let host = server.address().address
+
+  console.log('I am a running server', host, port)
+})
